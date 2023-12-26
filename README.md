a6caa473b69c0e078fbac3d81d7b37265a871cfa
Christian Meyer <chritsch@gmail.com>
2023-12-26 18:22:49 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,44 @@
+#  class SwissRandom
+The class generates a (random) set of coordinates in the common Swiss LV95 system within the borders of Switzerland and/or checks if they are in the country.
+
+~~It also supports WSG84 and the obsolete LV03 coordinate system.~~ The class can also be used to check if existing coordinates are within Switzerland.
+
+## Swiss coordinate system
+The Swiss coordinate system (or Swiss grid) is a geographic coordinate system used in Switzerland
+and Liechtenstein for maps and surveying by the Swiss Federal Office of Topography (Swisstopo).
+
+<Read more: https://www.swisstopo.admin.ch/en/knowledge-facts/surveying-geodesy/reference-frames/local.html
+
+## List of extreme points in Switzerland
+| Points | LV95 E | LV95 N | WGS84 Lat | WGS84 Lon |
+| --- | --- | --- | --- | --- |
+| Northernmost point in Switzerland | 2'684'602 | 1'295'934 | 47.80845 | 8.56803 |
+| Most easterly point in Switzerland | 2'833'859 | 1'166'962 | 46.61296 | 10.49219 |
+| Southernmost point of Switzerland | 2'722'707 | 1'075'269 | 45.81796 | 9.01734 |
+| Westernmost point of Switzerland | 2'485'410 | 1'110'070 | 46.13236 | 5.95590 |
+
+>Source: [Wikipedia DE](https://de.wikipedia.org/wiki/Geographische_Extrempunkte_der_Schweiz)
+
+>Verified ~~and also converted to the outdated LV03 coordinate system~~ on https://map.geo.admin.ch/
+
+## Missing features:
+
+- A set of user-defined minimum and maximum coordinates to limit the result to a specific area.
+- Support for WSG84 and the old LV03 coordinate systems.
+- Conversion between the three coordinate systems.
+
+## Shape file
+File: *..data/swissBOUNDARIES3D_1_5_TLM_LANDESGEBIET.shp*
+
+Source: [Bundesamt für Landestopografie swisstopo](https://www.swisstopo.admin.ch/de/geodata/landscape/boundaries3d.html)
+
+Downloaded: 26. December 2023
+
+## Requirements
+
+- geopandas==0.14.1
+- shapely==2.0.2
+
+## Version
+
+Version 0.5 / chme / 2023-12-26
---
+++ lv95_random.py
@@ -0,0 +1,120 @@
+import geopandas as gpd
+from shapely.geometry import Point
+import random
+
+
+# Generates a (random) set of coordinates within the borders of Switzerland and/or checks if they are in the country.
+class SwissRandom:
+    def __init__(self, coordinate_e: float = 0, coordinate_n: float = 0):
+        # Bounding boxes of Switzerland (and Liechtenstein) in LV95, LV03, and WGS84
+        self.coord_boxes = {
+            "LV95": {
+                "min_e": 2485409,
+                "min_n": 1075268,
+                "max_e": 2833860,
+                "max_n": 1295935,
+            },
+            "LV03": {
+                "min_e": 485410,
+                "min_n": 75270,
+                "max_e": 833858,
+                "max_n": 295935,
+            },
+            "WGS84": {
+                "min_e": 5.95590,
+                "min_n": 45.81796,
+                "max_e": 10.49219,
+                "max_n": 47.80845,
+            },
+        }
+
+        # Read the shape file
+        self.shape_ch = gpd.read_file("data/swissBOUNDARIES3D_1_5_TLM_LANDESGEBIET.shp")
+
+        self.set_coord_system("LV95")
+        self.coordinate_e = coordinate_e
+        self.coordinate_n = coordinate_n
+
+    def __str__(self) -> str:
+        return f"{self.__coord_system}: {self.coordinate_e}, {self.coordinate_n}"
+
+    # Set the coordinate system
+    def set_coord_system(self, coord_system: str):
+        if coord_system == "" or coord_system is None:
+            self.__coord_system = "LV95"
+        # elif coord_system in self.coord_boxes.keys():
+        #     self.__coord_system = coord_system
+        if coord_system.upper() == "LV95":
+            self.__coord_system = coord_system.upper()
+        else:
+            raise ValueError(
+                f"Coordinate system {coord_system} is not supported. Use one of {self.coord_boxes.keys()}"
+            )
+
+    # Get the coordinate system
+    def get_coord_system(self) -> str:
+        return self.__coord_system
+
+    # Get the bounding box of the coordinate system
+    def get_coord_box(self) -> dict:
+        return self.coord_boxes[self.__coord_system]
+
+    # Get the east coordinate
+    def get_coord_east(self) -> float:
+        return self.coordinate_e
+
+    # Get the north coordinate
+    def get_coord_north(self) -> float:
+        return self.coordinate_n
+
+    # Get the coordinates
+    def get_coords(self) -> tuple:
+        return (self.coordinate_e, self.coordinate_n)
+
+    # Get random coordinates within the bounding box of the coordinate system
+    def generate_coords(self):
+        # Generate random east coordinates
+        self.coordinate_e = random.uniform(
+            self.coord_boxes[self.__coord_system]["min_e"],
+            self.coord_boxes[self.__coord_system]["max_e"],
+        )
+        # Generate random north coordinates
+        self.coordinate_n = random.uniform(
+            self.coord_boxes[self.__coord_system]["min_n"],
+            self.coord_boxes[self.__coord_system]["max_n"],
+        )
+
+        # Check if both coordinates are within Switzerland
+        if self.check_coords():
+            return (self.coordinate_e, self.coordinate_n)
+        else:
+            self.generate_coords()
+
+    # Check if the coordinates are within Switzerland
+    def check_coords(self) -> bool:
+        point = Point(float(self.coordinate_e), float(self.coordinate_n))
+        # Check if the point is within the shape file
+        if any(self.shape_ch.contains(point)):
+            return True
+        else:
+            return False
+
+
+if __name__ == "__main__":
+    # Create an instance of the class
+    sr = SwissRandom()
+
+    # Generate some random coordinates
+    print(
+        "Generate some random coordinates\nTravelling back and forth across Switzerland . . ."
+    )
+    coords = []
+    for c in range(1000):
+        sr.generate_coords()  # Generate random coordinates
+        coords.append(sr.get_coords())  # Get the coordinates
+
+    # Print the most and least coordinates of the generated coordinates
+    print("easternmost", max(coords, key=lambda x: x[0]))
+    print("westernmost", min(coords, key=lambda x: x[0]))
+    print("northernmost", max(coords, key=lambda x: x[1]))
+    print("southernmost", min(coords, key=lambda x: x[1]))
